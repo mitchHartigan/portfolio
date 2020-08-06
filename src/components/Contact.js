de48c7c81519c6aa4299711d29bdc9c77f229434
mitchhartigan <mitchhartigan@gmail.com>
2020-08-06 11:09:50 -0700

    fix: updated contact info

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,22 +1,34 @@
-import React, {Component} from 'react';
-import PageTitle from './PageTitle';
+import React, { Component } from "react";
+import PageTitle from "./PageTitle";
 
 export default class Contact extends Component {
-
-  render() { 
-    return ( 
+  render() {
+    return (
       <div>
         <PageTitle textStyle="contact-Header-Text">Contact Me</PageTitle>
-        <h2 className="contact-Sub-Text">0403 203 255</h2>
+        <h2 className="contact-Sub-Text">949 449 5453</h2>
         <h2 className="contact-Sub-Text">mitch.hartigan@gmail.com</h2>
         <h2 className="contact-Sub-Text">
-          <a className="contact-Link" href="https://github.com/mitchHartigan" rel="noopener noreferrer" target="_blank">github.com/mitchHartigan</a>
+          <a
+            className="contact-Link"
+            href="https://github.com/mitchHartigan"
+            rel="noopener noreferrer"
+            target="_blank"
+          >
+            github.com/mitchHartigan
+          </a>
         </h2>
         <h2 className="contact-Sub-Text">
-          <a className="contact-Link" rel="noopener noreferrer" target="_blank" href="https://www.linkedin.com/in/mitchell-hartigan-388323189/">linkedin.com/in/mitchell-hartigan/</a>
+          <a
+            className="contact-Link"
+            rel="noopener noreferrer"
+            target="_blank"
+            href="https://www.linkedin.com/in/mitchell-hartigan-388323189/"
+          >
+            linkedin.com/in/mitchell-hartigan/
+          </a>
         </h2>
       </div>
-     );
+    );
   }
 }
- 
